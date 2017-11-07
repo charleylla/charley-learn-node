2e26d652da883a4ebd112024b92ca6764a53cd01
Charley <charleylla@foxmail.com>
2017-11-07 23:23:16 +0800

    2017/11/07 回调函数

--- README.md
+++ README.md
@@ -1,20 +1,22 @@
-# npm 相关的操作
-## 更新 npm
+# Node 中的回调
+## 什么是回调
+函数的调用分为三类：同步调用，异步调用和回调。
+## 阻塞和非阻塞
+阻塞和非阻塞关注的是程序在等待函数的调用结果时的状态。
+- 阻塞：必须在前面的调用执行完成后再执行后面的调用
+- 非阻塞：不用等待前面的函数调用完成，可以先做其他的事情，当前面的函数调用完成之后，再回过头对该函数的调用结果进行处理
+## 非阻塞函数的处理
+对于非阻塞函数，一般需要为其传入一个回调函数，用来在操作执行完成后调用：
 ```
-// 对 npm 进行全局安装
-npm install npm@latest -g
+function asyncMethocCb(){
+    // readFile 方法接受一个回调函数作为参数，用来在文件被读取完成后进行调用
+    const fdata3 = fs.readFile("./README.md",(err,data)=>{
+        if(err){
+            console.log("Something wrong!")
+        }
+        console.log(data.toString());
+    })
+}
+
+asyncMethocCb()
 ```
-## 在 REPL 环境搜索包
-```
-// 使用 npm search 命令
-npm search express
-```
-## 使用帮助
-```
-// 查看 npm 所有命令
-npm help
-// 查看某个具体命令的帮助信息
-npm help install
-```
-## cnpm
-cnpm 的使用方法和 npm 一致
---
+++ callback.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+// 阻塞代码示例
+// readFileSync 方法是一个同步灯方法
+function syncMethod(){
+    const fdata1 = fs.readFileSync("./README.md")
+    // console.log 方法会在文件读取完成之后再调用
+    // console.log(fdata1);//输出十六进制格式的数据
+    console.log(fdata1.toString());//将十六进制灯数据转换成字符串输出
+}
+
+// syncMethod();//输出 README.md 文件的内容
+
+// 同步代码示例
+// readFile 是一个非阻塞函数
+function asyncMethod(){
+    const fdata2 = fs.readFile("./README.md")
+    // console.log 方法不会等待 readFile 方法执行完成
+    console.log(fdata2) // undefined
+}
+
+// asyncMethod()
+
+// 回调函数示例
+function asyncMethocCb(){
+    // readFile 方法接受一个回调函数作为参数，用来在文件被读取完成后进行调用
+    const fdata3 = fs.readFile("./README.md",(err,data)=>{
+        if(err){
+            console.log("Something wrong!")
+        }
+        console.log(data.toString());
+    })
+}
+
+asyncMethocCb()
+// asyncMethodCb 方法不是阻塞的
+// console.log 方法不会等待 asyncMethodCb 执行完成
+console.log("代码执行完毕！")

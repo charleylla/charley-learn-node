489c77f7693f44a57ef94474e036ca67aee74423
Charley <charleylla@foxmail.com>
2017-11-14 23:34:44 +0800

    2017/11/14 使用 request 和 cheerio 构建简单爬虫

--- README.md
+++ README.md
@@ -1,44 +1,27 @@
-# 使用 Express + Mysql 做的一个添加用户名的小 DEMO
-## 说明
-- 使用 swig 作为模板引擎
-- 使用 mysql 包连接数据库
-- 使用 jQuery 发送 AJAX 请求
-- 字段非空判断
-
-## 表结构
+# Node 爬虫系统
+## 一些依赖的包
+构建爬虫系统时，使用一些第三方的包可以简化我们的操作，这里介绍两个相关的包：request 和 cheerio，首先来安装它们：
 ```
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : 本地
-Source Server Version : 50717
-Source Host           : localhost:3306
-Source Database       : db_node_express
-
-Target Server Type    : MYSQL
-Target Server Version : 50717
-File Encoding         : 65001
-
-Date: 2017-11-13 16:45:55
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for `tb_user`
--- ----------------------------
-DROP TABLE IF EXISTS `tb_user`;
-CREATE TABLE `tb_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of tb_user
--- ----------------------------
-INSERT INTO `tb_user` VALUES ('9', '张三');
-INSERT INTO `tb_user` VALUES ('10', '张三');
-INSERT INTO `tb_user` VALUES ('13', '王麻子');
-
+yarn add request cheerio -S
 ```
+## request 的基本使用 —— 爬取百度内容
+基本使用如下：
+```
+request("http://www.baidu.com",(err,res,body) => {
+    if(err){
+        throw new Error("爬取失败")
+    }
+    if(res.statusCode == 200){
+      doSomething()
+    }
+});
+```
+## cheerio 的基本使用
+获取到页面的 DOM 结构只是开始，更多的工作是需要对页面 DOM 结构进行分析，提取所需要的信息。cheerio 这个库就可以帮助我们提取这些信息。
+
+使用 cheerio 时，需要先装载对应的 HTML 内容：
+```
+const $ = cheerio.load(body)
+...
+```
+这里使用 ```$``` 作为装载对象的变量名，是因为该对象上的方法和 jQuery 很相似，可以像操作 jQuery 一样的操作（获取）爬取的 HMTL 页面的 DOM 结构。 
--- app.js
+++ app.js
@@ -1,108 +1,66 @@
 const express = require("express")
 const app = express()
-// 页面路由
 const router = express.Router();
-// 接口路由
-const routerAPI = express.Router();
-const bodyParser = require("body-parser");
-// 引入 swig 模板
-const swig = require("swig");
-// 设置模板引擎
-app.engine("html", swig.renderFile);
-app.set("view engine", "html");
-app.set("views", __dirname + "/views");
+app.listen(8080);
 
-// 使用 body-parser
-app.use(bodyParser.urlencoded({ extended: false }))
+;(function(){
+    // 引入 request 
+    const request = require("request")
+    
+    router.get("/",(req,res) => {
+        // 访问首页时使用 request 请求百度
+        getBaidu((body) => {
+            // 直接输出网页内容，我们就可以看到百度了
+            res.send(body)
+        });
+    });
 
-// 设置静态文件夹
-app.use(express.static("public"))
-
-// 数据库连接
-
-const mysql = require("mysql");
-
-// 使用一对一连接
-// const conn = mysql.createConnection({
-//     host:"localhost",
-//     user:"root",
-//     password:"123456",
-//     database:"db_node_express"
-// });
-
-// 使用数据库连接池
-const conn = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"123456",
-    database:"db_node_express"
-});
-
-// 连接数据库
-// 当使用数据库连接池时，无需执行 connect 方法
-// conn.connect((err) => {
-//     if(err){
-//         throw new Error("mysql connect failed!")
-//     }
-//     console.log("数据库连接成功")
-// });
-
-// 主页路由
-router.get("/",(req,res) => {
-    // 渲染 views 下的 index.html
-    res.render("index")
-})
-
-// API 路由，REST 风格
-routerAPI.route("/user")
-    .post((req,res) => {
-        const user = req.body;
-        const { username } = user;
-        // 非空判断
-        if(!username){
-            res.status(400);
-            res.json({
-                code:0,
-                msg:"用户名不能为空"
-            })
-
-            throw new Error("用户名不能为空")
-        }
-        console.log(user)
-        // 向数据库插入数据
-        conn.query("INSERT INTO tb_user SET ?",user,(err,results,fields) => {
-            // 写入数据库出错
+    function getBaidu(fn){
+        request("http://www.baidu.com",(err,res,body) => {
             if(err){
-                console.log(err)
-                res.status(400);
-                res.json({
-                    code:0,
-                    msg:"Failed"
-                });
-                throw new Error("插入数据失败！")
+                throw new Error("爬取失败")
             }
-            // console.log("====================")
-            // console.log(results)
-            // console.log("====================")
-            // console.log(fields)
-            // console.log("====================")
-            // console.log(conn.sql)
-            res.json({
-                code:1,
-                msg:"Success"
-            })
-        })
-    })
-
-// 应用路由设定
-app.use("/",router);
-app.use("/api",routerAPI)
-
-// 错误处理
-app.use((err,req,res,next) => {
-    console.log(err.stack)
+            // 获取信息成功后调用回调函数
+            if(res.statusCode == 200){
+                fn(body)
+            }
+        });
+    }
+    app.use("/",router)
 })
+// ();
 
-app.listen(8080,() => {
-    console.log("服务已启动！");
-});
+;(function(){
+    const request = require("request")
+    // 引入 cheerio
+    const cheerio = require("cheerio");
+
+    router.get("/",(req,res) => {
+        // 访问首页时使用 request 请求百度
+        getBaidu(($,body) => {
+            // 这里获取百度顶部的菜单信息
+            const len = $("#u1>a").length;
+            res.json({
+                navLen:len
+            });
+        });
+    });
+
+    function getBaidu(fn){
+        request("http://www.baidu.com",(err,res,body) => {
+            if(err){
+                throw new Error("爬取失败")
+            }
+
+            if(res.statusCode == 200){
+                // 使用 cheerio 装载 HTML
+                const $ = cheerio.load(body)
+                // 装载后返回的对象拥有跟 jQuery 类似的方法
+                fn($,body)
+            }
+        });
+    }
+    app.use("/",router)
+})
+()
+;

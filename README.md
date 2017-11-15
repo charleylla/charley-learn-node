210b75a577b6ef8225d578c8d184f57ffd30f86f
Charley <charleylla@foxmail.com>
2017-11-15 17:21:10 +0800

    2017/11/15 Generator 初涉

--- README.md
+++ README.md
@@ -1,1 +1,200 @@
-# Promise
+# Generator
+## 状态机
+Generator 函数是一个状态机生成器，其内部保留了一系列的状态。
+
+执行 Generator 函数会返回一个迭代器对象，通过调用迭代器对象的 ```next``` 方法可以进行取得内部封装的状态。
+```
+function* gen(){
+    yield 1
+    yield 2
+    yield 3
+}
+
+const iter = gen();
+console.log(iter.next()) //{ value: 1, done: false }
+console.log(iter.next()) //{ value: 2, done: false }
+console.log(iter.next()) //{ value: 3, done: false }
+console.log(iter.next()) //{ value: undefined, done: true }
+```
+调用 ```gen``` 函数，返回了一个迭代器，每次调用迭代器的 ```next``` 方法都可以获取生成器内部生成的状态。在生成器函数内部，使用 ```yield``` 关键字来产出状态，在英语中，```yield``` 本身也具有“产出”之意。
+## yield
+调用 Generator 函数会生成一个迭代器对象（内部指针），每次调用迭代器对象指针会进行移动，这个移动的过程就是代码从一个 ```yield``` 向下一个 ```yield``` 执行的过程。
+
+因此可以利用 ```yield``` 的这个特性来暂停函数的执行。 
+```
+function* gen(){
+    console.log(1)
+    yield
+    console.log("=============")
+    console.log(2)
+    console.log("=============")
+    yield
+    console.log(3)
+}   
+
+const iter = gen();
+iter.next();
+iter.next();
+iter.next();
+```
+每次调用 ```next``` 方法时，会从原先暂停的地方接着执行。
+# yield 表达式
+每次调用 ```next``` 方法获取的 ```value``` 值，实际上就是 ```yield``` 后面的表达式的返回值。
+
+注：调用 ```next``` 方法获取的是 ```yield``` 关键字后面的表达式的值，而不是 ```yield``` 表达式的值。当我们说 ```yield``` 表达式的值时，我们是在说 ```yield``` 表达式整体的值。
+
+如下有个例子：
+```
+function* gen(){
+    yield 1
+    yield 1 + 1
+    yield 3 * 3
+}
+
+const iter = gen()
+console.log(iter.next()) //{ value: 1, done: false }
+console.log(iter.next()) //{ value: 2, done: false }
+console.log(iter.next()) //{ value: 9, done: false }
+```
+由此，我们可以将 ```yield``` 表达式的值赋值给一个变量，然后在下一个 ```yield``` 中使用这个变量：
+```
+function* gen(){
+    const first = yield 1
+    const second = yield first + 1
+    const third = yield second + 1
+}
+const iter = gen();
+console.log(iter.next()) //{ value: 1, done: false }
+console.log(iter.next()) //{ value: NaN, done: false }
+console.log(iter.next()) //{ value: NaN, done: false }
+```
+这就很尴尬了，为啥 ```value``` 中有 ```NaN``` 呢？
+
+造成 ```NaN``` 的原因是我们对 ```undefined``` 执行了加法操作。为什么会有 ```undefined``` 的出现呢？这是因为在默认情况下，```yield``` 表达式的值为 ```undefined```（注意 ```yield``` 表达式的值和 ```yield``` 后面表达式的值的区别，上面有做说明）。
+
+我们可以用如下代码验证，默认情况下 ```yield``` 表达式的值都是 ```undefiend```：
+```
+function* gen(){
+    const first = yield 1
+    console.log("first:",first)
+    const second = yield 1 + 1
+    console.log("second:",second)
+    const third = yield 2 + 1
+    console.log("third:",third)
+}
+const iter = gen();
+iter.next()
+iter.next()
+iter.next()
+iter.next()
+```
+上面代码的输出结果为：
+```
+first: undefined
+second: undefined
+third: undefined
+```
+现在我们知道了，调用 ```next``` 方法并不是获取的 ```yield``` 表达式的值，而是获取的 ```yield``` 关键字后面的表达式的值。
+
+知道了这一点，我们就可以来看 ```next``` 方法的参数了。
+## next 方法的参数
+```next``` 方法的参数的意义在于：**其参数的值会代替上一条 ```yield``` 表达式的值**。也就是说，```yield``` 表达式的值实际上是由 ```next``` 方法的参数来决定，默认情况下不传入参数，其值就是 ```undefined```。
+
+现在来看一下经过改进的例子：
+```
+function* gen(){
+    const first = yield 1
+    const second = yield first + 1
+    const third = yield second + 1
+}
+const iter = gen();
+console.log(iter.next(1)) //{ value: 1, done: false }
+console.log(iter.next(2)) //{ value: 3, done: false }
+console.log(iter.next(3)) //{ value: 4, done: false }
+console.log(iter.next(4)) //{ value: undefined, done: true }
+```
+值得注意的是，对于第一次调用 ```next``` 方法时传入的参数会被忽略，因为 ```next``` 方法的参数表示上一个 ```yield``` 表达式的值，在第一次调用 ```next``` 方法时是没有上一个 ```yield``` 表达式的，因此该参数会被忽略，传不传都可以。
+## throw 方法
+在迭代器外部，我们可以调用迭代器对象的 ```throw``` 向内部抛出错误，同时在迭代器内部使用 ```try...catch``` 进行错误捕获。
+```
+function* gen(){
+    const first = yield 1
+    let second;
+    // 迭代器内部可以使用 try...catch 进行错误捕获
+    try{
+        second  = yield first + 1
+    }catch(e){
+        console.log(second) // undefined
+        second = 3; 
+    }
+    const third = yield second + 1
+}
+const iter = gen();
+console.log(iter.next())
+console.log(iter.next(2))
+// 使用 throw 向迭代器内部抛出错误
+console.log(iter.throw(new Error("Error")))
+console.log(iter.next())
+```
+上面的代码中，第三次调用 ```next``` 方法时，本该对上一次的 ```yield``` 表达式的值进行加法操作，但我们使用 ```throw``` 方法手动抛出了一个错误，在迭代器内部如果不对该错误进行处理，错误会被全局作用域捕获，并中断后续代码的执行。
+
+我们这里对错误进行了处理，使得我们能够正确得到上一次 ```yield``` 表达式的值，因而我们的迭代操作能够正常执行。
+
+调用 ```next``` 和 ```throw``` 都可以使迭代器继续执行（如果 ```throw``` 抛出的错误被正确处理了的话）。
+
+## 委托生成器
+有时在一个生成器中可能需要使用别的生成器的内容，但又不想从头再写一次，就可以使用委托生成器。委托生成器的用法比较简单，一看便知：
+```
+function* genNum(){
+    yield 1
+    yield 2
+}
+
+function* genStr(){
+    yield "Hello"
+    yield "World"
+}
+
+function* gen(){
+    yield *genNum()
+    yield *genStr();
+}
+
+const str = gen();
+console.log(str.next())
+console.log(str.next())
+console.log(str.next())
+console.log(str.next())
+console.log(str.next())
+```
+运行结果为：
+```
+{ value: 1, done: false }
+{ value: 2, done: false }
+{ value: 'Hello', done: false }
+{ value: 'World', done: false }
+{ value: undefined, done: true }
+```
+### 将委托生成器应用于可迭代对象
+前面的应用的都是都是我们手动定义的迭代器，我们也可以将委托生成器应用在一些内置的可迭代对象上，这样就会调用这些内置可迭代对象的迭代方法：
+```
+function* gen(){
+    yield *"Hello"
+}
+const iter = gen();
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
+```
+上面代码的运行结果为：
+```
+{ value: 'H', done: false }
+{ value: 'e', done: false }
+{ value: 'l', done: false }
+{ value: 'l', done: false }
+{ value: 'o', done: false }
+{ value: undefined, done: true }
+```
--- app.js
+++ app.js
@@ -1,399 +1,151 @@
 ;(function(){
-    console.log(1)
-    new Promise(res => {
-        res();
-    }).then(resData => {
-        // then 相当于 Promise 的回调函数，会在当前同步线程运行完成之后再进行
-        console.log(2)
-    });
-    console.log(3)
-    // 输出 1 3 2
-})
-// ();
-
-
-// relove 一个 promise 对象，实现状态替换
-;(function(){
-    // 在 resolve 时可以传入另一个 Promise 对象
-    const p1 = new Promise((res,rej) => {
-        setTimeout(() => {
-            res("Success P1");
-        }, 2000);
-    });
-
-    // p2 resolve 了 p1，那么 p2 的状态就决定于 p1 的状态
-    // 此时 p2 的状态就变成了 p1 的状态，引入 p2 的 then 中的 res 参数就是 p1 resolve 的参数
-    const p2 = new Promise((res,rej) => {
-        setTimeout(() => {
-            res(p1)
-        },1000)
-    })
-
-    p2.then((res) => {
-        console.log(res)
-    }).catch((err) => {
-        console.log(err)
-    })
-})
-// ();
-
-// resolve 或者 reject 后并不会中断当前函数的执行
-;(function(){
-    new Promise(res => {
-        res("hello");
-        console.log("memeda")
-    }).then(data => {
-        console.log(data);
-    })
-    // 输出 memeda hello
-    // resolve 或者 reject 后并不会中断当前函数的执行
-})
-// ();
-
-// 如果在 resolve 或者 reject 之前 return，那么就不会发生状态改变，也就不会执行 then 了
-;(function(){
-    new Promise(res => {
-        return false;
-        res("hello")
-    }).then(data => {
-        console.log(data)
-    })
-})
-// ();
-
-;(function(){
-    const p = new Promise(res => {
-        setTimeout(() => {
-            res( {code:1,msg:"Success"} )
-        }, 500);
-    });
-
-    p.then(data => {
-        console.log(data)
-        // 返回第二个 Promise，就可以链式调用 then 方法
-        // 在 then 中返回另一个 Promise 对象，就可以在该 Promise 对象状态改变后调用下一个 then 中的回调函数
-        // 如果 resolve，就会调用 then 中的 resolve 回调
-        // 如果 reject，就会调用 then 中的 reject 回调
-
-        // 当我们要处理多重回调时，就在状态改变后，在 then 中返回另一个 Promise，这样就避免了多重的回调嵌套
-        // 后一个 then 中的回调函数，会在前一个 Promsie 的状态改变后再调用
-        return new Promise((res,rej) => {
-            setTimeout(() => {
-                const flag = Math.random() > 0.5;
-                if(flag) res("Success");
-                else rej("Failed")
-            }, 1000);
-        })
-    }).then((succData) => {
-        console.log(succData)
-    },(failData) => {
-        console.log(failData)
-    });
-})
-// ();
-
-// 使用 catch 捕获错误
-;(function(){
-    const p = new Promise(res => {
-        // 在 Promise 内部抛出一个错误
-        throw new Error("Error!")
-    })
-
-    // 使用 catch 捕获错误
-    p.catch(err => {
-        console.log(err)
-    })
-})
-// ();
-
-// 使用 then(null,err) 捕获错误
-;(function(){
-    const p = new Promise(res => {
-        // 在 Promise 内部抛出一个错误
-        throw new Error("Error!")
-    })
-
-    // 使用 then(null,err) 捕获错误
-    p.then(null,err => {
-        console.log(err)
-    })
-
-    // 因此，catch(err) 方法相当于 then(null,err) 的一个简写
-})
-// ();
-
-// 两种抛出错误的方式
-;(function(){
-    const p1 = new Promise((res,rej) => {
-        // 在 Promise 内部使用 try...catch 代码块捕获错误
-        try{
-            throw new Error("Error in p1")
-        }catch(e){
-            // 使用 reject 向外传递错误
-            rej(e)
-        }
-    })
-    // 使用 catch 捕获 p1 中的错误
-    p1.catch(err => {
-        console.log(err)
-    });
-
-    const p2 = new Promise((res,rej) => {
-        // 在 Promise 中直接抛出错误，就可以在 catch 中捕获
-        throw new Error("Error in p2")
-    });
-    p2.catch(err =>{
-        console.log(err)
-    });
-
-    // 上面两种方式都可以用来抛出错误，第二种方式更为简洁一些
-})
-// ();
-
-// 状态改变后再抛出错误无效
-;(function(){
-    const p = new Promise(res => {
-        res();
-        throw new Error("Error")
-    })
-
-    p.catch(err => {
-        console.log(err)
-    })
-})
-// ();
-
-// 不使用 then 的第二个回调函数是一种最佳实践
-;(function(){
-    const p = new Promise((res,rej) => {
-        rej("Error in p")
-    }).then(data => {
-        // then 中的第一个回调函数只用来处理 resolve 的数据
-        // 不建议 then 中的第二个（失败）回调函数
-        console.log(data)
-    }).catch(err => {
-        // 错误处理放在 catch 中
-        console.log("Error:")
-        console.log(err)
-    })
-})
-// ();
-
-// 在 catch 中返回一个 Promise 对象，状态改变后仍然可以执行后续的 then 方法
-;(function(){
-    const p = new Promise((res,rej) => {
-        rej("Error in p")
-    }).then(data => {
-        console.log(data)
-    }).catch(err => {
-        console.log("Error:")
-        console.log(err);
-        // 在 catch 中返回一个 Promise 对象
-        // 用以在抛出异常后进行重新请求啥的
-        return new Promise((res,rej) => {
-            res("Success in catch Promise")
-        })
-    }).then(res => {
-        console.log(res)
-    })
-
-    // 一般而言，一个 then 中紧接一个 catch，用以进行异常捕获
-})
-// ();
-
-;(function(){
-    const p = new Promise((res,rej) =>{
-        const flag = Math.random() > 0.5;
-        if(flag) res("Success");
-        else rej("Falied")
-    }).then(data => {
-        console.log("1",data)
-        // 当前面的 Promsie 状态 resolve 会执行此回调
-        // 该回调执行后会返回一个新的 Promsie 对象，该对象会被后面的 then 捕获
-        return new Promise((res,rej) => {
-            res("Success 2");
-        })
-    }).catch(err => {
-        // 虽然没有明确的返回一个 Promise，但是当 catch 到错误后，仍会导致后面的 then 捕获
-        console.log("0",err)
-    }).then((data) => {
-        console.log("1",data)
-    });
-    // 此段代码输出的两种结果：
-    /**
-     * 0 Falied
-     * 1 undefined
-     * 或：
-     * 1 Success
-     * 1 Success 2
-     */
-})
-// ();
-
-// Promise.all 方法
-// 该方法用接受多个 Promsie 对象，返回一个被包装的新的 Promsie
-// 当内部所有的 Promsie 都 resolve 或者有一个 Promsie 变为 reject 后，这个新包装的 Promise 对象才会发生状态改变
-// 通常可以用此方法来进行并行请求
-;(function(){
-    const p1 = new Promise((res,rej) => {
-        setTimeout(() => {
-            const flag = Math.random() > 0.5;
-            if(flag) res("Data in p1");
-            else rej("Error in p1");
-        },1000)
-    });
-
-    const p2 = new Promise((res,rej) => {
-        setTimeout(() => {
-            const flag = Math.random() > 0.5;
-            if(flag) res("Data in p2");
-            else rej("Error in p2");
-        },2000)
-    })
-
-    // 使用 Promise.all 包装一个新对象
-    const pAll = Promise.all([p1,p2])
-    // 当内部的所有 Promise 对象都 resolve 或者有一个 reject 后，pAll 的状态发生变化
-    // pAll 的 resolve 回调函数的参数为一个数组，其中保存了内部 Promsie 对象 resolve 的值
-    pAll.then(([data1,data2]) => {
-        console.log(data1)
-        console.log(data2)
-    }).catch((err) => {
-        // 当有一个 Promsie 变成 reject 时，就会触发 catch 中的回调
-        console.log(err)
-    })
-})
-// ();
-
-// Promsie.race 方法
-// 同 Promise.all 方法，该方法接受多个 Promise 数组，返回一个经过包装的 Promsie
-// 顾名思义，这是内部的多个 Promise 进行竞赛，只要有一个 Promise 发生了状态改变，这个经过包装的 Promsie 就会发生状态改变
-// 该方法可以用来在验证某个 API 请求是否在某个时间段内完成，如果完成了就 resolve，否则就 reject
-// 贴上阮一峰的代码：
-;`
-const p = Promise.race([
-    fetch('/resource-that-may-take-a-while'),
-    new Promise(function (resolve, reject) {
-        setTimeout(() => reject(new Error('request timeout')), 5000)
-    })
-]);
-p.then(response => console.log(response));
-p.catch(error => console.log(error));
-`
-;(function(){
-    const p1 = new Promise((res,rej) => {
-        setTimeout(() => {
-            res("P1 Success")
-        },2000);
-    });
-
-    const p2 = new Promise((res,rej) => {
-        setTimeout(() => {
-            res({code:-1,msg:"Timeout!"})
-        },1000);
-    });
-
-    const pRace = Promise.race([p1,p2]);
-    pRace.then(data => {
-        if(data.code === -1){
-            console.log(data.msg)
-            return;
-        }
-        console.log(data)
-    });
-    pRace.catch(err => {
-        console.log("Error:",err)
-    });
-})
-// ();
-
-// Promise.resolve 方法
-// 该方法用来将一个普通对象装换为 Promise 对象
-// 关于 Promise.resolve 方法，接受的参数，有几种不同的情况
-;(function(){
-    // 1.参数为 Promsie 对象
-    // 当参数为 Promise 对象时，原封不动的返回该对象
-    const p1  = new Promise(res => res("p1 Success"));
-    const p1Transfer = Promise.resolve(p1);
-    p1Transfer.then(data => {
-        console.log(data)
-    })
-    console.log(p1 === p1Transfer);
-
-    // 2.参数是一个带有then 方法的对象
-    // 将该对象转换为 Promsie 对象，并立即执行该对象的 then 方法
-    const thenable = {
-        then(res,rej){
-            setTimeout(() => {
-                res("Success thenable")
-            },1000);
-        }
+    function* gen(){
+        yield 1
+        yield 2
+        yield 3
     }
-    const thenableTransfer = Promise.resolve(thenable);
-    thenableTransfer.then(data => {
-        console.log(data)
-    });
 
-    // 3.参数是不具有 then 方法的对象，或者不是对象
-    // 返回一个理解 resolve 的对象，resolve 的参数就是这个参数对象
-    const otherObj = {a:1}
-    const otherObjTransfer = Promise.resolve(otherObj)
-    otherObjTransfer.then(data => {
-        console.log(data)
-    });
-
-    // 4.不带任何参数
-    // 返回一个立即 resolve 的对象
-    // 这个立即 resolve 是在本次事件循环结束时执行，而不是在下一轮循环开始时执行
-    // setTimeout(fn,0) 是在下一轮循环开始时执行
-    const resolvePromise = Promise.resolve();
-    setTimeout(() => {
-        console.log("=====>下次事件循环开始时执行")
-    }, 0);
-    resolvePromise.then(() => {
-        console.log("=====>本次事件循环结束前执行")
-    })
-
-    // 执行结果：
-    /**
-     * =====>本次事件循环结束前执行
-     * =====>下次事件循环开始时执行
-     */
-
-    // 使用 Promise.resolve 创建的立即 resolve 的对象，会在本次事件循环结束时执行，因此先输出
-    // 使用 setTimeout(fn,0) 会在下次事件循环开始时执行，因此后输出
+    const iter = gen();
+    console.log(iter.next())
+    console.log(iter.next())
+    console.log(iter.next())
+    console.log(iter.next())
 })
 // ();
 
-// Promsie.reject 方法
-// 该方法返回一个立即 reject 的 Promise 对象
-// 和 Promise.resolve 对象不同的是，传递给 Promise.reject 的任何参数都会原封不动的传入 catch 方法中
 ;(function(){
-    const message = {
-        code:-1,
-        msg:"Failed"
-    };
-    
-    const pReject = Promise.reject(message);
+    function* gen(){
+        console.log(1)
+        yield
+        console.log("=============")
+        console.log(2)
+        console.log("=============")
+        yield
+        console.log(3)
+    }   
 
-    pReject.catch(err => {
-        console.log(err)
-        console.log(err === message)
-    })
+    const iter = gen();
+    iter.next();
+    iter.next();
+    iter.next();
 })
 // ();
 
-// 不管同步还是异步的方法，都可以采用 promise 的方式去执行。
-// 通过 then 指定下一步执行的方法。
 ;(function(){
-    console.log("Start")
-    const f = () => console.log("Hello Promise.");
-    (async () => {
-        f();
-    })()
-    .then((data) => {
-        console.log(data)
-    });
-    console.log("End")
+    function* gen(){
+        yield 1
+        yield 1 + 1
+        yield 3 * 3
+    }
+
+    const iter = gen()
+    console.log(iter.next()) //{ value: 1, done: false }
+    console.log(iter.next()) //{ value: 2, done: false }
+    console.log(iter.next()) //{ value: 9, done: false }
+})
+// ();
+
+;(function(){
+    function* gen(){
+        const first = yield 1
+        const second = yield first + 1
+        const third = yield second + 1
+    }
+    const iter = gen();
+    console.log(iter.next()) //{ value: 1, done: false }
+    console.log(iter.next()) //{ value: NaN, done: false }
+    console.log(iter.next()) //{ value: NaN, done: false }
+})
+// ();
+
+;(function(){
+    function* gen(){
+        const first = yield 1
+        console.log("first:",first)
+        const second = yield 1 + 1
+        console.log("second:",second)
+        const third = yield 2 + 1
+        console.log("third:",third)
+    }
+    const iter = gen();
+    iter.next()
+    iter.next()
+    iter.next()
+    iter.next()
+})
+// ();
+
+;(function(){
+    function* gen(){
+        const first = yield 1
+        const second = yield first + 1
+        const third = yield second + 1
+    }
+    const iter = gen();
+    console.log(iter.next(1)) //{ value: 1, done: false }
+    console.log(iter.next(2)) //{ value: 3, done: false }
+    console.log(iter.next(3)) //{ value: 4, done: false }
+    console.log(iter.next(4)) //{ value: undefined, done: true }
+})
+// ();
+
+;(function(){
+    function* gen(){
+        const first = yield 1
+        let second;
+        // 迭代器内部可以使用 try...catch 进行错误捕获
+        try{
+            second  = yield first + 1
+        }catch(e){
+            console.log(second) // undefined
+            second = 3; 
+        }
+        const third = yield second + 1
+    }
+    const iter = gen();
+    console.log(iter.next()) //{ value: 1, done: false }
+    console.log(iter.next(2)) //{ value: 3, done: false }
+    // 使用 throw 向迭代器内部抛出错误
+    console.log(iter.throw(new Error("Error"))) //{ value: 4, done: false }
+    console.log(iter.next()) //{ value: undefined, done: true }
+})
+// ();
+
+;(function(){
+    function* genNum(){
+        yield 1
+        yield 2
+    }
+
+    function* genStr(){
+        yield "Hello"
+        yield "World"
+    }
+
+    function* gen(){
+        yield *genNum()
+        yield *genStr();
+    }
+
+    const str = gen();
+    console.log(str.next())
+    console.log(str.next())
+    console.log(str.next())
+    console.log(str.next())
+    console.log(str.next())
+})
+// ();
+
+;(function(){
+    function* gen(){
+        yield *"Hello"
+    }
+    const iter = gen();
+    console.log(iter.next())
+    console.log(iter.next())
+    console.log(iter.next())
+    console.log(iter.next())
+    console.log(iter.next())
+    console.log(iter.next())
 })
 ();
